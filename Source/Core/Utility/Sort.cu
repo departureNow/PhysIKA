9a026df7d7c0bd91e76e4c12960b173ec46b4535
clouddon <clouddon@sina.com>
2021-01-22 15:17:03 +0800

    Fix compilation problem

--- Source/Core/Utility/Sort.cu
+++ Source/Core/Utility/Sort.cu
@@ -20,6 +20,48 @@ namespace PhysIKA {
 	Sort::~Sort() {
 		cudaFree(num);
 	}
+
+	__device__ int getBinaryByN(int num, int id) {
+		int count = 0;
+		int temp;
+		while (num != 0) {
+			temp = num % 2;
+			num = num / 2;
+			count++;
+			if (count == id) {
+				return temp;
+			}
+		}
+		return 0;
+	}
+
+	__global__ void deviceRadixSort(int *arr, int length) {
+		__shared__ int a0[MAX_LENGTH];
+		int id = threadIdx.x;
+		int k0 = 0;
+		int k1 = 0;
+// 		for (int i = 0; i < length; i++) {
+// 			int x = getBinaryByN(arr[i], id + 1);
+// 			if (x == 0) {
+// 				a0[k0] = arr[i];
+// 				k0++;
+// 			}
+// 			else if (x == 1) {
+// 				a1[k1] = arr[i];
+// 				k1++;
+// 			}
+// 			__syncthreads();
+// 		}
+// 		for (int i = 0; i < k0; i++) {
+// 			arr[i] = a0[i];
+// 		}
+// 		__syncthreads();
+// 		for (int i = k0, i1 = 0; i < k0 + k1; i1++, i++) {
+// 			arr[i] = a1[i1];
+// 		}
+		__syncthreads();
+	}
+
 	void Sort::radixSort(int * arr, int length) {
 		numMalloc(arr, length);
 		int maxNum = INT_MIN;
@@ -41,48 +83,6 @@ namespace PhysIKA {
 		}
 		return count;
 	}
-
-	__device__ int getBinaryByN(int num, int id) {
-		int count = 0;
-		int temp;
-		while (num != 0) {
-			temp = num % 2;
-			num = num / 2;
-			count++;
-			if (count == id) {
-				return temp;
-			}
-		}
-		return 0;
-	}
-
-	__global__ void deviceRadixSort(int *arr, int length) {
-		extern __shared__ int a0[MAX_LENGTH];
-		extern __shared__ int a1[MAX_LENGTH];
-		int id = threadIdx.x;
-		int k0 = 0;
-		int k1 = 0;
-		for (int i = 0; i < length; i++) {
-			int x = getBinaryByN(arr[i], id + 1);
-			if (x == 0) {
-				a0[k0] = arr[i];
-				k0++;
-			}else if (x == 1) {
-				a1[k1] = arr[i];
-				k1++;
-			}
-			__syncthreads();
-		}
-		for (int i = 0; i < k0; i++) {
-			arr[i] = a0[i];
-		}
-		__syncthreads();
-		for (int i = k0, i1 = 0; i < k0 + k1; i1++, i++) {
-			arr[i] = a1[i1];
-		}
-		__syncthreads();
-	}
-
 
 	void Sort::numMalloc(int *arr, int length) {
 		cudaMalloc((void**)&num, length * sizeof(int));
